f193eba52a704daf3115c417e6f27ab371f6a929
Vladimir Kempik <vladimir.kempik@syntacore.com>
2025-01-23 13:53:00 +0300

    Amdgpu support with atomic modesetting disabled

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2258,7 +2258,10 @@ static int amdgpu_pci_probe(struct pci_dev *pdev,
 	if (amdgpu_virtual_display ||
 	    amdgpu_device_asic_has_dc_support(flags & AMD_ASIC_MASK))
 		supports_atomic = true;
-
+#if IS_ENABLED(CONFIG_SOC_SPACEMIT_K1X)
+	if (!amdgpu_virtual_display) // atomic modesetting is not working on amdgpu&spacemit
+		supports_atomic=false;
+#endif
 	if ((flags & AMD_EXP_HW_SUPPORT) && !amdgpu_exp_hw_support) {
 		DRM_INFO("This hardware requires experimental hardware support.\n"
 			 "See modparam exp_hw_support\n");
